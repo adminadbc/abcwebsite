55c7330ed4bf8d51dc3a9389a03ac0b8fcf2835e
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-11 15:33:24 -0600

    delete fontawesome

--- app/consutrction.tsx
+++ app/consutrction.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Typography } from "@material-tailwind/react";
+
 import Image from "next/image";
 
 export function MaintenanceSection4() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import MainPage from "./main/page";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
 
 export default function Home() {
   return (
