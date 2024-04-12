406f304a1e9dd7ec7c45441a59388f6f8ba9f1b8
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-12 09:21:44 -0600

    newsletter style change

--- app/ui/main/newsletter.tsx
+++ app/ui/main/newsletter.tsx
@@ -5,22 +5,22 @@ import { Button, Input, Typography } from "@material-tailwind/react";
 export function NewsLetter1() {
   return (
     <section className="mt-10 p-10">
-      <div className="container mx-auto flex !w-full max-w-6xl flex-col !items-center justify-center rounded-2xl bg-gray-900 px-6 py-16">
+      <div className="container mx-auto flex !w-full max-w-6xl flex-col !items-center justify-center rounded-2xl bg-white px-6 py-16">
         <Typography
           className="text-center text-2xl font-bold md:text-3xl "
-          color="white"
+          color="black"
         >
           Be the first who see the news
         </Typography>
         <Typography
-          color="white"
+          color="black"
           className="my-3 text-center !text-base md:w-7/12"
         >
           Your company may not be in the software business, but eventually, a
           software company will be in your business.
         </Typography>
         <div className="mt-2 flex w-full flex-col gap-3 md:w-fit md:flex-row">
-          <Input label="Email" color="white" crossOrigin={undefined} />
+          <Input label="Email" color="black" crossOrigin={undefined} />
           <Button size="md" className="flex-shrink-0 bg-abcf text-white">
             button
           </Button>
