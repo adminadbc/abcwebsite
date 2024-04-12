4fc1b59d1eb7734486432623628bd0aea49347c8
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-12 09:01:23 -0600

    projects and testimonials

--- app/ui/main/foundermessage.tsx
+++ app/ui/main/foundermessage.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { Typography } from "@material-tailwind/react";
 
 export function FounderMessage() {
   return (
@@ -8,56 +9,66 @@ export function FounderMessage() {
       <div className="text-center my-12">
         <h2 className="text-4xl lg:text-5xl ">Founder&#39;s Message</h2>
 
-        <div className="flex-grow border-t-4 border-abcf w-1/2 mx-auto mt-2"></div>
+        <div className="relative flex py-5 w-1/2  mx-auto items-center">
+          <div className="flex-grow border-t-4 border-abcf w-96"></div>
+          <p className="mx-5 w-48 text-center">Ultimate Change</p>
+          <div className="flex-grow border-t-4 border-abcf w-96"></div>
+        </div>
       </div>
       <div className="flex flex-row-reverse flex-wrap-reverse max-w-7xl mx-auto my-auto items-center place-items-center justify-around">
-        <div className="lg:max-w-md">
-          <h2
-            color="blue-gray"
-            className="mb-6 lg:text-3xl text-2xl font-semibold lg:max-w-md"
-          >
-            So what does the new record for the lowest level of winter ice
-            actually mean
-          </h2>
-          <p className="text-xl font-normal mb-12 text-gray-800">
-            The Arctic Ocean freezes every winter and much of the sea-ice then
-            thaws every summer, and that process will continue whatever happens
-            with climate change. Even if the Arctic continues to be one of the
-            fastest-warming regions of the world, it will always be plunged into
-            bitterly cold polar dark every winter. And year-by-year, for all
-            kinds of natural reasons, there&apos;s huge variety of the state of
-            the ice.
-            <br />
-            <br />
-            For a start, it does not automatically follow that a record amount
-            of ice will melt this summer. More important for determining the
-            size of the annual thaw is the state of the weather as the midnight
-            sun approaches and temperatures rise. But over the more than 30
-            years of satellite records, scientists have observed a clear pattern
-            of decline, decade-by-decade.
-          </p>
-          <p className="text-xl mb-8 font-normal text-gray-800">
-            The Arctic Ocean freezes every winter and much of the sea-ice then
-            thaws every summer, and that process will continue whatever happens
-            with climate change. Even if the Arctic continues to be one of the
-            fastest-warming regions of the world, it will always be plunged into
-            bitterly cold polar dark every winter. And year-by-year, for all
-            kinds of natural reasons, there&apos;s huge variety of the state of
-            the ice.
-          </p>
-          <Link href="" className="mt-9 bg-abcf px-20 text-white text-lg py-3">
-            {" "}
-            Read More{" "}
-          </Link>
-        </div>
-        <div>
+        <div className="inline m-5 container  my-auto text-center">
           <Image
             src="/angie-lal.jpg"
-            alt="team work"
-            className="mb-6 h-full lg:h-[750px] w-full shadow-lg"
-            width={370}
+            alt="blog"
+            className="float-left mr-10 mb-5 rounded-lg shadow-2xl lg:max-w-[26rem] lg:min-h-[26rem]"
+            width={500}
             height={500}
           />
+
+          <div className="mb-12 text-justify">
+            <Typography className="mb-8 font-normal !text-gray-600">
+              At ABC Foundation, we are fueled by a passionate commitment to
+              justice and equality. Our mission is clear: to promote access to
+              justice, amplify marginalized voices, and educate individuals
+              about their legal rights. We believe that by empowering
+              communities with knowledge and resources, we can pave the way for
+              fair and just outcomes for all.
+            </Typography>
+
+            <Typography className="mb-8 font-normal !text-gray-600">
+              In today&quot;s world, the path to justice can often seem daunting
+              and inaccessible, particularly for those facing socio-economic
+              challenges or systemic barriers. That&quot;s where we come in. Our
+              dedicated team is here to provide support, guidance, and advocacy
+              every step of the way. Whether it&quot;s navigating the
+              complexities of the legal system or standing up for your rights,
+              we are here to ensure that no one is left behind.
+            </Typography>
+
+            <Typography className="mb-6 font-normal !text-gray-600">
+              Our vision for a better Canada is one where justice knows no
+              bounds. We envision a society where everyone, regardless of their
+              background or circumstances, can access the legal support they
+              need. But our vision doesn&quot;t stop there. We believe in the
+              power of communities to drive real change. That&quot;s why
+              we&quot;re committed to fostering active engagement and
+              collaboration, empowering individuals to become advocates for
+              systemic reform and equity.
+            </Typography>
+            <Typography className="mb-6 font-normal !text-gray-600">
+              As we embark on this journey together, we invite you to join us in
+              our mission. Together, we can build a future where justice is not
+              just a privilege, but a fundamental right for all.
+            </Typography>
+            <Typography className="mb-6 font-normal !text-gray-600">
+              Prior to founding ABC Foundation, I held various roles in real
+              estate, oil & gas, finance, automotive, and construction sectors,
+              honing my skills in project management, client relations, and
+              organizational efficiency. My diverse background has equipped me
+              with a keen understanding of community needs and a holistic
+              approach to problem-solving.
+            </Typography>
+          </div>
         </div>
       </div>
     </section>
--- app/ui/main/seperatoone.tsx
+++ app/ui/main/seperatoone.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function WidgetsExample6() {
   return (
     // CHANGED THE ALIGNMENT OF THE WIDGET TO CENTER BASE OF THE VIEW
-    <div className="rounded-xl bg-gradient-to-r from-abcf to-yellow-700  py-14 my-16 px-10 md:px-28 text-center">
+    <div className="rounded-xl bg-gradient-to-r from-abcf to-orange-400  py-14 my-16 px-10 md:px-28 text-center">
       <h3 className="font-bold text-2xl md:text-4xl w-2/3 mx-auto mb-10">
         Lorem ipsum dolor, sit amet consectetur adipisimos illatis vel
         aspernatur.
