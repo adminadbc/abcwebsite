5dfd9cf1ab4d69c60511814bf8a81f6653c3c8ab
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-12 12:30:54 -0600

    booking calendar color change

--- app/ui/booking/calendar.tsx
+++ app/ui/booking/calendar.tsx
@@ -18,6 +18,26 @@ export default function MyApp() {
         styles: { branding: { brandColor: "#B49712" } },
         hideEventTypeDetails: false,
         layout: "month_view",
+        cssVarsPerTheme: {
+          light: {
+            "cal-brand": "#B49712",
+            "cal-text": "#B49712",
+            "cal-text-emphasis": "#B49712",
+            "cal-border-emphasis": "#B49712",
+            "cal-text-error": "pink",
+            "cal-border": "#B49712",
+            "cal-border-default": "#B49712",
+            "cal-border-subtle": "#B49712",
+            "cal-border-booker": "#B49712",
+            "cal-text-muted": "#a28810",
+            "cal-bg-emphasis": "#efd55d",
+            // More CSS variables are defined here
+            // https://github.com/calcom/cal.com/blob/b0ca7dae1a17f897e34b83c990f30ab65f615ee0/packages/config/tailwind-preset.js#L69
+          },
+          dark: {
+            // Set the similar variables as in light theme but for dark mode.
+          },
+        },
       });
     })();
   }, []);
