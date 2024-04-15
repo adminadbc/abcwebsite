fbf27669b5c628528bbf8ad907cf911544cc4100
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-15 12:03:33 -0600

    booking page ui chnage

--- app/ui/booking/contact.tsx
+++ app/ui/booking/contact.tsx
@@ -1,81 +1,12 @@
 "use client";
 
 import React from "react";
-import {
-  Card,
-  CardBody,
-  IconButton,
-  Typography,
-} from "@material-tailwind/react";
-import {
-  EnvelopeIcon,
-  MapPinIcon,
-  PhoneIcon,
-} from "@heroicons/react/24/outline";
-
-interface ContactCardProps {
-  icon: React.ReactNode;
-  title: string;
-  description: React.ReactNode;
-  contact: string;
-}
-
-function ContactCard({ icon, title, description, contact }: ContactCardProps) {
-  return (
-    <Card color="transparent" shadow={false}>
-      <CardBody className="grid justify-center text-center">
-        <IconButton
-          size="lg"
-          className="mx-auto place-items-center rounded-full bg-abcf"
-        >
-          {icon}
-        </IconButton>
-        <Typography
-          variant="h5"
-          color="blue-gray"
-          className="my-4 font-semibold"
-        >
-          {title}
-        </Typography>
-        <Typography className="mb-4 font-normal !text-gray-500">
-          {description}
-        </Typography>
-        <Typography color="blue-gray" className="font-medium">
-          {contact}
-        </Typography>
-      </CardBody>
-    </Card>
-  );
-}
-
-const options = [
-  {
-    icon: <MapPinIcon className="h-6 w-6" strokeWidth={2} />,
-    title: "Address",
-    description: "Find us at the office",
-    contact: "12124 First Street, nr 54",
-  },
-  {
-    icon: <EnvelopeIcon className="h-6 w-6" strokeWidth={2} />,
-    title: "Email",
-    description: "Send us your feedback",
-    contact: "info@abcfoundationconnect.com",
-  },
-  {
-    icon: <PhoneIcon className="h-6 w-6" strokeWidth={2} />,
-    title: "Phone",
-    description: "Give us a ring",
-    contact: "+1 (424) 535-3523",
-  },
-];
+import { Typography } from "@material-tailwind/react";
 
 export function ContactSection1() {
   return (
-    <section className="px-8 py-10 lg:py-28">
+    <section className="px-15 py-10">
       <div className="container mx-auto mb-20 text-center">
-        <Typography color="blue-gray" className="mb-4 font-bold">
-          Contact Us
-        </Typography>
         <Typography
           variant="h1"
           color="blue-gray"
