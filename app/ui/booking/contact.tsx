f658871e396f35496b8fb675e47ec1213c6f8389
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-14 23:39:56 -0600

    booking page font change

--- app/ui/booking/contact.tsx
+++ app/ui/booking/contact.tsx
@@ -84,7 +84,7 @@ export function ContactSection1() {
           We Are Here To Help
         </Typography>
         <div className="flex-grow border-t-4 border-abcf w-2/5 mx-auto mt-2 mb-7"></div>
-        <Typography variant="lead" className="mx-auto !text-gray-500">
+        <Typography variant="lead" className="mx-auto ">
           We&apos;d like to talk more about what you need
         </Typography>
       </div>
