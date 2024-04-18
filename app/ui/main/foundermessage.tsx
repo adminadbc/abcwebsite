fa33267d522fd380831429cee382d8dcfe20040e
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-18 16:22:27 -0600

    text change

--- app/ui/main/foundermessage.tsx
+++ app/ui/main/foundermessage.tsx
@@ -49,23 +49,22 @@ export function FounderMessage() {
             At ABC Foundation, we are fueled by a passionate commitment to
             justice and equality. Our mission is clear: to promote access to
             justice, amplify marginalized voices, and educate individuals about
-            their legal rights. We believe that by empowering communities with
-            knowledge and resources, we can pave the way for fair and just
-            outcomes for all.
+            their legal rights. By empowering communities with knowledge and
+            resources, we can pave the way for fair and just outcomes for all.
             <br />
             <br />
-            In today&#x2019;s world, the path to justice can often seem daunting
-            and inaccessible, particularly for those facing socio-economic
-            challenges or systemic barriers. That&#x2019;s where we come in. Our
-            dedicated team is here to provide support, guidance, and advocacy
-            every step of the way. Whether it&#x2019;s navigating the
-            complexities of the legal system or standing up for your rights, we
-            are here to ensure that no one is left behind.
+            The path to justice can often seem daunting and inaccessible today,
+            particularly for those facing socio-economic challenges or systemic
+            barriers. That&#x2019;s where we come in. Our dedicated team is here
+            to provide support, guidance, and advocacy every step of the way.
+            Whether navigating the complexities of the legal system or standing
+            up for your rights, we are here to ensure that no one is left
+            behind.
             <br />
             <br />
             Our vision for a better Canada is one where justice knows no bounds.
-            We envision a society where everyone, regardless of their background
-            or circumstances, can access the legal support they need. But our
+            We envision a society where everyone can access the legal support
+            they need regardless of their background or circumstances. But our
             vision doesn&#x2019;t stop there. We believe in the power of
             communities to drive real change. That&#x2019;s why we&#x2019;re
             committed to fostering active engagement and collaboration,
@@ -73,9 +72,9 @@ export function FounderMessage() {
             equity.
             <br />
             <br />
-            As we embark on this journey together, we invite you to join us in
-            our mission. Together, we can build a future where justice is not
-            just a privilege, but a fundamental right for all.
+            As we embark on this journey, we invite you to join us in our
+            mission. Together, we can build a future where justice is not just a
+            privilege but a fundamental right for all.
           </p>
           <div className="flex flex-col mb-10 mt-1">
             <Image
