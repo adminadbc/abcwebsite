24e8f3b8c741978253e2e05842b3346b41f4911d
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-18 20:45:00 -0600

    text alignment

--- app/ui/changemaker/body.tsx
+++ app/ui/changemaker/body.tsx
@@ -53,7 +53,7 @@ export function ContentSection11() {
               height={500}
             />
           </div>
-          <div className="flex flex-col gap-8 text-justify">
+          <div className="flex flex-col gap-8">
             <Option icon={CloudIcon} title="Unity Through Inclusion:">
               ABC Foundation&#x2019;s Changemakers initiative provides a
               fortified and inclusive platform for influential voices and
