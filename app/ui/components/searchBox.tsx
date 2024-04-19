62d62313119440995328fe12a8897c9f498ce6cf
sideeq12 <abdwaheed2018@gmail.com>
2024-04-19 13:14:59 +0100

    whole search

--- app/ui/components/searchBox.tsx
+++ app/ui/components/searchBox.tsx
@@ -7,7 +7,7 @@ import { useState,useEffect } from "react"
 const API_KEY = process.env.API_KEY;
 const userId = process.env.USER_ID;
 // Connect and authenticate with your Algolia app
-const client = algoliasearch(API_KEY!, userId!)
+const client = algoliasearch("","")
 
 
 const appId = process.env.APP_iD!;
@@ -49,11 +49,12 @@ function SearchLayer() {
     <div>
   {
     pop ?  <div className="absolute top-0 w-screen overflow-x-hidden
-       h-screen z-[100]  bg-blue-300 left-0 py-20  md:pt-32  px-10 md:px-[10%] backdrop-blur-md bg-opacity-60 bg-blur-lg">
+       h-screen z-[100]  bg-blue-300 left-0 py-20  md:pt-32  px-10 md:px-[10%] backdrop-blur-md bg-opacity-30
+        bg-blur-sm">
               <input type="text" name="name" className="z-9999 h-16 w-3/4 text-center
                 outline-blue-500 mb-6 md:mb-10  mx-[10%]"
                   onChange={(e)=>{handleSearch(e)}} placeholder="search..." />
-                            {<div className="flex gap-2 flex-wrap mx-auto relative">
+                            {<div className="flex gap-2 flex-wrap mx-auto relative justify-center align-middle">
        {resultList?.map((listData, idx)=><Link  target="_blank" href="https://medium.com/swlh/3-subtle-signs-youve-hired-a-great-lawyer-e72a65010792" 
        key={idx} className="flex
            z-[999] bg-white max-h-40 flex-row-reverse gap-2 w-full md:w-1/2 lg:w-1/3  overflow-hidden ">
