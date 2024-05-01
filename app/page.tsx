ddea546286189e7030c1fec9ccd91b2fdaaa75ac
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-30 20:12:05 -0600

    fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import MainPage from "./main/page";
-import Navbar from "../app/ui/components/navbar"
+import Navbar from "../app/ui/components/navbar";
 import { fetchPublication } from "./ui/components/getHashnode";
-import { fetchVideos } from "./ui/components/updateAlgolia"
+import { fetchVideos } from "./ui/components/updateAlgolia";
 
 export default function Home() {
-  fetchPublication()
-  fetchVideos()
+  fetchPublication();
+  fetchVideos();
   return (
     <div className="w-screen overflow-hidden">
       <Navbar />
