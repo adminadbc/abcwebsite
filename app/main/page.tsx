51d0e47c78548a5da6896b1c383838afe2d79d94
sideeq12 <abdwaheed2018@gmail.com>
2024-05-02 12:12:23 +0100

    animation

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -17,13 +17,48 @@ import LogoCarousel from "../ui/logocarousel/logoCarousel";
 import { Footer4 } from "../ui/components/footer";
 import NavBar from "../ui/components/navbar";
 
+
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
 function MainPage() {
+  const controlsFirst = useAnimation();
+  const controlsSecond = useAnimation();
+  const controlsThird = useAnimation();
+  const [refFirst, inViewFirst] = useInView();
+  const [refSecond, inViewSecond] = useInView();
+  const [refThird, inViewThird] = useInView();
+
+  useEffect(() => {
+    if (inViewFirst) {
+      controlsFirst.start({ opacity: 1, y: 0 , transition: { delay: 0.5} });
+    }
+  }, [controlsFirst, inViewFirst]);
+
+  useEffect(() => {
+    if (inViewSecond) {
+      controlsSecond.start({ opacity: 1, y: 0 , transition: { delay: 0.5 }});
+    }
+  }, [controlsSecond, inViewSecond]);
+
+  useEffect(() => {
+    if (inViewThird) {
+      controlsThird.start({ opacity: 1, y: 0 ,   transition: { delay: 0.5 } });
+    }
+  }, [controlsThird, inViewThird]);
   return (
     <div className="w-screen overflow-x-hidden">
-      {/* <NavBar /> */}
+       <motion.div ref={refFirst} initial={{ opacity: 0, y: 50 }} animate={controlsFirst}>
       <HeroSection12 />
+      </motion.div>
+      {/* <NavBar /> */}
+      <motion.div ref={refSecond} initial={{ opacity: 0, y: 50 }} animate={controlsSecond}>
       <FeatureSection3 />
+      </motion.div>
+      <motion.div ref={refThird} initial={{ opacity: 0, y: 50 }} animate={controlsThird}>
       <DonateSeperator />
+      </motion.div>
       <FeatureSection14 />
       <WidgetsExample6 />
       <FounderMessage />
