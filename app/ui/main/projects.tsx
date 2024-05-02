ceb5bf5537d3a96df02b2698ebb724835494c87f
sideeq12 <abdwaheed2018@gmail.com>
2024-05-02 20:19:26 +0100

    compl

--- app/ui/main/projects.tsx
+++ app/ui/main/projects.tsx
@@ -3,17 +3,35 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@material-tailwind/react";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 export function ProjectSection() {
 
+  const [refFirst, inViewFirst] = useInView();
+  const [refSecond, inViewSecond] = useInView();
+    const controlsSecond = useAnimation();
+  const controlsFirst = useAnimation();
+    useEffect(() => {
+      if (inViewFirst) {
+        controlsFirst.start({ opacity: 1, y: 0 , transition: { delay: 0.5, duration : 0.5} });
+      }
+    }, [controlsFirst, inViewFirst]);
   
+      useEffect(() => {
+      if (inViewSecond) {
+        controlsSecond.start({ opacity: 1, y: 0 , transition: { delay: 0.8, duration : 0.5 }});
+      }
+    }, [controlsSecond, inViewSecond]);
   return (
     <section className="my-10 flex gap-4">
       <div
         className=" bg-gradient-to-r bg-brand2  py-20 px-10 md:pl-24 lg:pl-10 flex 
       flex-wrap items-center justify-evenly gap-6 w-screen text-white"
       >
-        <div className="lg:w-1/3 space-y-7  overflow-hidden">
+         {/* <motion.div ref={refFirst} initial={{ opacity: 0, y: -50 }} animate={controlsFirst} exit={{ opacity: 0, y: 50 }}> */}
+          <div className="lg:w-1/3 space-y-7  overflow-hidden">
           <p>Join us at ABC Foundation to make a change</p>
           <h3 className="text-4xl font-semibold">Our Current Projects</h3>
           <div className="flex-grow border-t-4 border-abcf w-96"></div>
@@ -24,7 +42,11 @@ export function ProjectSection() {
             for updates on our upcoming initiatives!
           </p>
         </div>
+      {/* </motion.div> */}
+        
         <div className="flex items-stretch flex-wrap justify-evenly gap-6">
+         
+          <motion.div ref={refFirst} initial={{ opacity: 0, y: -50 }} animate={controlsFirst} exit={{ opacity: 0, y: 50 }}>
           <div className="bg-white rounded-lg p-3 drop-shadow-lg text-center">
             <Image
               src="/law.jpeg"
@@ -45,7 +67,9 @@ export function ProjectSection() {
               </Link>
             </div>
           </div>
-          <div className="bg-white rounded-lg p-3 drop-shadow-lg text-center">
+      </motion.div>
+      <motion.div ref={refFirst} initial={{ opacity: 0, y: -50 }} animate={controlsFirst} exit={{ opacity: 0, y: 50 }}>
+      <div className="bg-white rounded-lg p-3 drop-shadow-lg text-center">
             <Image
               src="/lawspot.jpeg"
               width={250}
@@ -65,6 +89,7 @@ export function ProjectSection() {
               </Link>
             </div>
           </div>
+      </motion.div>
         </div>
       </div>
     </section>
