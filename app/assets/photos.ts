77eb5d6d040de9fbf18190908b6131b526de4f55
Rashad Bayram <BRashad@users.noreply.github.com>
2024-07-19 12:20:35 -0600

    installed gallery

---
+++ app/assets/photos.ts
@@ -0,0 +1,39 @@
+
+
+export const photos = [
+  {
+    src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    title: "A view of the city from the hill",
+    description: "Photo one",
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    title: "A view of the city from the hill",
+    description: "Photo two",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1720884413532-59289875c3e1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZyZWUlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D",
+    title: "A view of the city from the hill",
+    description: "Photo three",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1709884735017-114f4a31f944?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    title: "A view of the city from the hill",
+    description: "Photo four",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1709884732294-90379fee354c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    title: "A view of the city from the hill",
+    description: "Photo five",
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1661889099855-b44dc39e88c9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    title: "A view of the city from the hill",
+    description: "Photo six",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1544894079-e81a9eb1da8b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    title: "A view of the city from the hill",
+    description: "Photo seven",
+  },
+];
--- app/main/about/gallery/page.tsx
+++ app/main/about/gallery/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import NavBar from "../../../ui/components/navbar";
+
+import Hero from "../../../ui/gallery/hero";
 import Footer from "../../../ui/components/footer";
 
-function page() {
+function GalleryPage() {
   return (
     <div>
       <NavBar />
-      
+      <Hero />
+
       <Footer />
     </div>
   );
 }
 
-export default page
+export default GalleryPage;
