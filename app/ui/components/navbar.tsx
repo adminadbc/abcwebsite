27d0f710c1ec93fbabe12d35cb199c90a45a8928
Rashad Bayram <BRashad@users.noreply.github.com>
2024-10-02 15:50:26 -0600

    all new navbar items and link created

--- app/ui/components/navbar.tsx
+++ app/ui/components/navbar.tsx
@@ -1,42 +1,105 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { Navbar, Collapse, IconButton, Button } from "@material-tailwind/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import Custom from "./dropDown";
-
 import { FaSquareFacebook, FaLinkedin } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
 import SearchLayer from "./searchBox";
 
+interface DropMenuProps {
+  title: string;
+  items: { name: string; href: string }[];
+}
+
+const DropMenu: React.FC<
+  DropMenuProps & { isOpen: boolean; toggleDropdown: () => void }
+> = ({ title, items, isOpen, toggleDropdown }) => {
+  return (
+    <div
+      className="flex flex-col text-left"
+      onMouseEnter={toggleDropdown}
+      onMouseLeave={toggleDropdown}
+    >
+      <div
+        className="inline-flex justify-start w-full py-2 bg-white font-normal"
+        aria-haspopup="true"
+        {...(isOpen && { "aria-expanded": "true" })}
+      >
+        <h6 className="mr-3">{title}</h6>
+      </div>
+      <div
+        className={`absolute md:mt-10 w-[200px] rounded-md shadow-lg mt-10 bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 ${
+          isOpen ? "" : "hidden"
+        }`}
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
+        <div className="py-1" role="none">
+          {items.map((item, index) => (
+            <a
+              key={index}
+              href={item.href}
+              className="block px-4 py-2 text-base hover:text-abcf"
+              role="menuitem"
+            >
+              <h6>{item.name}</h6>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const links = [
   { name: "Home", href: "/main" },
   {
-
-    href: "/main/about/team/founder",
+    name: "About Us",
+    href: "/about",
     submenu: true,
-    submenuItems: [{ name: "Founder", href: "/main/about/team/founder" }],
+    submenuItems: [
+      { name: "Team", href: "/about/team" },
+      { name: "Gallery", href: "/about/gallery" },
+    ],
   },
   {
     name: "Resources",
     href: "/main/resources",
     submenu: true,
     submenuItems: [
-      { name: "Articles", href: "/articles" },
-      { name: "Education", href: "/education" },
+      { name: "Education", href: "/main/resources/education" },
+      { name: "Articles", href: "https://blog.abcfoundationconnect.com/" },
     ],
   },
-
+  {
+    name: "Initiatives",
+    href: "/education",
+    submenu: true,
+    submenuItems: [
+      {
+        name: "Legal Awareness Program",
+        href: "/education/legal-awareness-program",
+      },
+      { name: "Changemakers", href: "/education/changemakers" },
+    ],
+  },
   { name: "Contact Us", href: "/contacts" },
 ];
 
 export function NavBar() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen((cur) => !cur);
+
+  const [openDropdown, setOpenDropdown] = useState<number | null>(null);
+  const toggleDropdown = (dropdownId: number) => {
+    setOpenDropdown(openDropdown === dropdownId ? null : dropdownId);
+  };
 
   const pathname = usePathname();
 
@@ -66,21 +129,27 @@ export function NavBar() {
         </Link>
 
         <div className="mx-auto hidden items-center gap-6 xl:flex text-xl">
-          {links.map((link, idx) =>
-            link.name == "Resources" ? (
-              <Custom key={8} />
-            ) : (
-              <Link
-                key={idx}
-                href={link.href}
-                className={clsx({
-                  "bg-sky-100 text-abcf": pathname === link.href,
-                })}
-              >
-                <h6 className="hidden md:block">{link.name}</h6>
-              </Link>
-            )
-          )}
+          {links.map((link, idx) => (
+            <React.Fragment key={idx}>
+              {link.submenu ? (
+                <DropMenu
+                  title={link.name}
+                  items={link.submenuItems}
+                  isOpen={openDropdown === idx}
+                  toggleDropdown={() => toggleDropdown(idx)}
+                />
+              ) : (
+                <Link
+                  href={link.href}
+                  className={clsx({
+                    "bg-sky-100 text-abcf": pathname === link.href,
+                  })}
+                >
+                  <h6 className="hidden md:block">{link.name}</h6>
+                </Link>
+              )}
+            </React.Fragment>
+          ))}
         </div>
 
         <div className="lg:mr-24 ml-[33%] md:ml-[65%] xl:ml-2">
@@ -115,18 +184,13 @@ export function NavBar() {
       <Collapse open={open}>
         <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
           <ul className="flex flex-col gap-4 text-gray-900 text-lg">
-            <li className="hover:text-abcf">
-              <Link href="/main">
-                <h6>Home</h6>
-              </Link>
-            </li>
-            
-            <Custom />
-            <li className="hover:text-abcf -mt-2">
-              <Link href="/main/contacts">
-                <h6>Contact Us</h6>
-              </Link>
-            </li>
+            {links.map((link, idx) => (
+              <li key={idx} className="hover:text-abcf">
+                <Link href={link.href}>
+                  <h6>{link.name}</h6>
+                </Link>
+              </li>
+            ))}
           </ul>
           {/* <SearchLayer /> */}
           <Link href="https://donate.abcfoundationconnect.com/b/8wMaEK1aw8OGdj2144">
