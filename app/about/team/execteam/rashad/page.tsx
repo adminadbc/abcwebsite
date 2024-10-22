f58802e776f38dca007a615e29fb1dccba2bf72e
Rashad Bayram <BRashad@users.noreply.github.com>
2024-10-21 22:29:59 -0600

    exec team link fix

--- app/about/team/execteam/rashad/page.tsx
+++ app/about/team/execteam/rashad/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import NavBar from "../../../../../ui/components/navbar";
-import { HeroSection } from "../../../../../ui/about/hero";
-import { Footer4 } from "../../../../../ui/components/footer";
+import NavBar from "@/app//ui/components/navbar";
+import { HeroSection } from "@/app//ui/about/hero";
+import { Footer4 } from "@/app//ui/components/footer";
 
-import { BlogSection14 } from "../../../../../ui/about/execteam/rashad";
+import { BlogSection14 } from "@/app//ui/about/execteam/rashad";
 
 function FounderPage() {
   return (
